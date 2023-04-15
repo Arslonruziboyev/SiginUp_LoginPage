54d8a9652ed95ac1eea8a9cc904ba52cdcf242c2
02arslon <arslonruziboyev@gmail.com>
2023-04-15 19:48:32 +0900

    changed

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,33 @@
+import { useState } from "react"
+
 const Login = () =>{
+    const[username, usernameupdate] = useState('');
+    const[password, passwordupdate] = useState('');
+
+    const ProceedLogin = (e) =>{
+        e.preventDefault();
+
+    }
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="row">
+            <div className="offset-lg-3 col-lg-6">
+                <form onSubmit={ProceedLogin}>
+                  <div className="card">
+                    <div className="card-header">
+                        <h2>User Login</h2>
+                    </div>
+                    <div className="card-body">
+                        <div className="form-group">
+                            <label>User Name <span className="errmsg">*</span></label>
+                            <input className="form-control"></input>
+                        </div>
+                    </div>
+                    <div className="card-footer">
+
+                    </div>
+                  </div>
+                </form>
+            </div>
         </div>
     )
 }
