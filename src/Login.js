668037138871e9213236ec23d31cdc40e057b12b
02arslon <arslonruziboyev@gmail.com>
2023-04-15 19:54:21 +0900

    changed

--- src/Login.js
+++ src/Login.js
@@ -1,35 +1,54 @@
-import { useState } from "react"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Login = () =>{
-    const[username, usernameupdate] = useState('');
-    const[password, passwordupdate] = useState('');
+const Login = () => {
+  const [username, usernameupdate] = useState("");
+  const [password, passwordupdate] = useState("");
 
-    const ProceedLogin = (e) =>{
-        e.preventDefault();
-
-    }
-    return (
-        <div className="row">
-            <div className="offset-lg-3 col-lg-6">
-                <form onSubmit={ProceedLogin}>
-                  <div className="card">
-                    <div className="card-header">
-                        <h2>User Login</h2>
-                    </div>
-                    <div className="card-body">
-                        <div className="form-group">
-                            <label>User Name <span className="errmsg">*</span></label>
-                            <input className="form-control"></input>
-                        </div>
-                    </div>
-                    <div className="card-footer">
-
-                    </div>
-                  </div>
-                </form>
+  const ProceedLogin = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="row">
+      <div className="offset-lg-3 col-lg-6">
+        <form onSubmit={ProceedLogin}>
+          <div className="card">
+            <div className="card-header">
+              <h2>User Login</h2>
             </div>
-        </div>
-    )
-}
+            <div className="card-body">
+              <div className="form-group">
+                <label>
+                  User Name <span className="errmsg">*</span>
+                </label>
+                <input
+                  value={username}
+                  onChange={(e) => usernameupdate(e.target.value)}
+                  className="form-control"
+                ></input>
+              </div>
+              <div className="form-group">
+                <label>
+                  Password <span className="errmsg">*</span>
+                </label>
+                <input
+                  value={password}
+                  onChange={(e) => passwordupdate(e.target.value)}
+                  className="form-control"
+                ></input>
+              </div>
+            </div>
+            <div className="card-footer">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+              <Link className="btn btn-success">New User</Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
